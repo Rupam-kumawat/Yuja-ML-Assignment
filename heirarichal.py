e7c52d8eec5914c0e4e33150fa8d5b650b495b79
Rupam-kumawat <140946358+Rupam-kumawat@users.noreply.github.com>
2025-01-08 13:36:14 +0530

    Update heirarichal.py

--- heirarichal.py
+++ heirarichal.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 def hierarchical_clustering(data, max_clusters=50):
     best_n_clusters = None
     best_score = -1
+    best_labels=None
     for n_clusters in range(2, max_clusters):
-        clustering = AgglomerativeClustering(n_clusters=n_clusters, linkage='ward', affinity='euclidean')
+        clustering = AgglomerativeClustering(n_clusters=n_clusters, linkage='ward', metric='euclidean')
         labels = clustering.fit_predict(data)
 
         if len(set(labels)) > 1 and len(set(labels)) < len(data):
@@ -15,13 +16,14 @@ def hierarchical_clustering(data, max_clusters=50):
             if score > best_score:
                 best_n_clusters = n_clusters
                 best_score = score
+                best_labels=labels
     
-    return best_n_clusters, best_score
+    return best_n_clusters, best_score, best_labels
 
 def visualize_hierarchical(data, labels, n_clusters):
     plt.figure(figsize=(10, 7))
     plt.scatter(data[:, 0], data[:, 1], c=labels, cmap='viridis', marker='o', edgecolor='k', alpha=0.7)
     plt.title(f'Hierarchical Clustering with {n_clusters} Clusters')
-    plt.xlabel('Feature 1 (scaled)')
-    plt.ylabel('Feature 2 (scaled)')
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
     plt.show()
